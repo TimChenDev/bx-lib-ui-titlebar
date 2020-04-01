96b544afd4683f9fbca9600215790139fd9a744d
TimChen <RSTimChen@gmail.com>
2020-04-01 16:28:21 +0800

    feat: support resource input

--- lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
+++ lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
@@ -3,3 +3,4 @@ package co.bxvip.tools.toast
 import android.app.Activity
 
 inline fun Activity.toastMe(message: CharSequence) = ToastUtils.show(message)
+inline fun Activity.toastMe(message: Int) = ToastUtils.show(message)
